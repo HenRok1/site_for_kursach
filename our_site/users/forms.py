0940fa16fc874042e3973b156ea274a76c995d71
roberter <roberter@student.21-school.ru>
2022-12-20 19:50:12 +0300

    lk is done

--- our_site/users/forms.py
+++ our_site/users/forms.py
@@ -9,7 +9,7 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'password1', 'password2']
 
 
 class UserUpdateForm(forms.ModelForm):
@@ -17,7 +17,7 @@ class UserUpdateForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username']
 
 
 class ProfileUpdateForm(forms.ModelForm):
