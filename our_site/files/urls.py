40a0e786c4e6187291a3fc8a58f314ed1c6c61d0
roberter <roberter@student.21-school.ru>
2022-12-25 21:52:08 +0300

    some done for our site

--- our_site/files/urls.py
+++ our_site/files/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from .views import upload_resume, SearchResultsView
 
-
 urlpatterns = [
-    path('',upload_resume, name = "files"),
+    path('', upload_resume, name="files"),
     path('search_results/', SearchResultsView.as_view(), name='search_results'),
 ]
--- our_site/our_site/urls.py
+++ our_site/our_site/urls.py
@@ -20,8 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('get_docs.urls')),
@@ -32,7 +30,6 @@ urlpatterns = [
     path('upload/', include('files.urls')),
     path('fillin/', include('kurs.urls')),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
